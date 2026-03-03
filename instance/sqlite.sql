192ad3a7f3c664dfd169280981a630090af2226f
Paul Gideon Wabwire <paulgideonw@gmail.com>
2026-03-03 19:37:06 +0000

    Add files via upload

---
+++ instance/sqlite.sql
@@ -0,0 +1,23 @@
+-- SQLite
+SELECT * FROM job ORDER BY created_at DESC;
+
+SELECT COUNT(*) AS total_jobs FROM job;
+
+SELECT * FROM job 
+WHERE DATE(created_at) = DATE('now');
+
+SELECT title, budget, is_negotiable 
+FROM job 
+WHERE budget IS NOT NULL 
+ORDER BY budget DESC;
+
+-- Get all cleaning jobs sorted by urgency
+SELECT title, description, urgency, location 
+FROM job 
+WHERE category = 'Cleaning'
+ORDER BY 
+  CASE urgency 
+    WHEN 'High' THEN 1 
+    WHEN 'Medium' THEN 2 
+    ELSE 3 
+  END;
